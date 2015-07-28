366d9f3ac4bb5e617283bad065d9959d64b0002d
hvallee <hugo.v.vallee@gmail.com>
2015-07-28 13:20:35 +0200

    save pcm and display it in the iframe

--- content.js
+++ content.js
@@ -6,14 +6,19 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "find_tables" ) {
 
-            var editor = '<iframe src="http://opencompare.org:/embedPCM/55968380975a03aaae9945a3?enableEdit=false&enableExport=false&enableTitle=false&enableShare=false" ' +
-                'scrolling="no"  width="100%" height="600px" style="border:none;"></iframe>';
 
             var firstTable = $("table").clone().wrap('<table>').parent().html(); // without wrapping <table>, we only select the innerHTML
-           // $("table").replaceWith(editor);
-            console.log(firstTable);
 
             chrome.runtime.sendMessage({"message": "convert_to_editor", "table": firstTable});
+        }
+
+        if( request.message === "replace_table" ) {
+            console.log("dsdqsdqsdsq");
+            var editor = '<iframe src="http://localhost:9000/embedPCM/'+request.id+'?enableEdit=false&enableExport=false&enableTitle=false&enableShare=false" ' +
+                'scrolling="no"  width="100%" height="600px" style="border:none;"></iframe>';
+
+            $("table").replaceWith(editor);
+
         }
     }
 );
--- popup.js
+++ popup.js
@@ -14,8 +14,21 @@ chrome.runtime.onMessage.addListener(
             fd.append('title', 'Test');
             fd.append('productAsLines', true);
             var req = new XMLHttpRequest();
-            req.open("POST", "http://localhost:9000/api/import/html");
+            req.open("POST", "http://localhost:9000/api/embedFromHtml");
             req.send(fd);
+
+            req.onreadystatechange=function(){
+                if (req.readyState==4 && req.status==200){
+                    // Send a message to the active tab
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        var activeTab = tabs[0];
+                        chrome.tabs.sendMessage(activeTab.id, {
+                            "message": "replace_table",
+                            "id": req.responseText
+                        });
+                    });
+                }
+            }
         }
     }
 );
