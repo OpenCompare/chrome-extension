8c9e869a1d04c46dc9f6f3ef1d0fbd512eaead12
hvallee <hugo.v.vallee@gmail.com>
2015-07-29 13:32:17 +0200

    Button to stop checking page

--- content.js
+++ content.js
@@ -2,23 +2,39 @@
  * Created by hvallee on 7/23/15.
  */
 
-
 chrome.runtime.onMessage.addListener(
 
     function(request, sender, sendResponse) {
         var id = 0;
+
         if( request.message === "find_tables" ) {
+
+            if (window.location.protocol == "https:") {
+                chrome.runtime.sendMessage({
+                    "message": "displayHttpsInstructions"
+                });
+                // We add an iFrame to display the shield icon
+                var editor = '<iframe src="http://localhost:9000/embedPCM" height="0"></iframe>';
+
+                $("body").append(editor);
+            }
 
             var tables =$("table");
 
             for(var index = 0; index < tables.length; index++) {
                 var table = tables[index];
+                console.log(table);
 
                 var button = document.createElement("button");
+                button.setAttribute("class", "waves-effect waves-light btn");
+                button.setAttribute("style", "margin-top: 10px");
                 button.setAttribute("id", index.toString());
+                button.setAttribute("data-type", 'OpenCompareButton');
 
                 button.addEventListener('click', function(event) {
+
                     id = event.target.getAttribute("id");
+                    $('#'+id).hide();
 
                     chrome.runtime.sendMessage({
                         "message": "convert_to_editor",
@@ -31,40 +47,20 @@ chrome.runtime.onMessage.addListener(
                 var parentDiv = tables[index].parentNode;
                 parentDiv.insertBefore(button, tables[index]);
             }
-
-
+            console.log(tables.length);
         }
 
-        if( request.message === "replace_table" ) {
-            if (window.location.protocol == "https:") {
-                chrome.runtime.sendMessage({
-                    "message": "displayHttpsInstructions",
-                    "table": tables[id].outerHTML,
-                    "index": id
-                });
-            }
-            else{
-                var editor = '<iframe src="http://localhost:9000/embedPCM/'+request.id+'?enableEdit=false&enableExport=false&enableTitle=false&enableShare=false" ' +
+        if( request.message === "remove_buttons" ) {
+            var buttons =$("button[data-type='OpenCompareButton']");
+            buttons.remove();
+        }
+
+            if( request.message === "replace_table" ) {
+                var editor = '<iframe src="http://localhost:9000/embedPCM/' + request.id + '?enableEdit=false&enableExport=false&enableTitle=false&enableShare=false" ' +
                     'scrolling="no"  width="100%" height="600px" style="border:none;"></iframe>';
 
                 $("table").eq(request.tableIndex).replaceWith(editor);
-            }
-
-
         }
     }
 );
 
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('0');
-
-    checkPageButton.addEventListener('click', function() {
-
-        // Send a message to the active tab
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            var activeTab = tabs[0];
-            chrome.tabs.sendMessage(activeTab.id, {"message": "find_tables"});
-        });
-
-    }, false);
-}, false);
--- popup.js
+++ popup.js
@@ -3,14 +3,48 @@
  */
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    $(document).ready(function(){
+        $( '#gotIt' ).click(function() {
+            $( '#message' ).hide();
+        });
+    });
+
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function() {
 
         // Send a message to the active tab
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            document.getElementById('checkPage').style.display = 'none';
+            document.getElementById('uncheckPage').style.display = 'block';
             var activeTab = tabs[0];
             chrome.tabs.sendMessage(activeTab.id, {"message": "find_tables"});
         });
 
     }, false);
+
+    var uncheckPageButton = document.getElementById('uncheckPage');
+    uncheckPageButton.addEventListener('click', function() {
+
+        // Send a message to the active tab
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            document.getElementById('checkPage').style.display = 'block';
+            document.getElementById('uncheckPage').style.display = 'none';
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {"message": "remove_buttons"});
+        });
+
+    }, false);
 }, false);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "displayHttpsInstructions") {
+            $( '#message' ).show();
+        }
+        if (request.message === "hideHttpsInstructions") {
+            $( '#message' ).hide();
+        }
+
+    }
+);
